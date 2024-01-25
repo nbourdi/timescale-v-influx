02e8df5a73893be5ea21db94e4f0d56910297b70
Natalia Bourdi <62358292+nbourdi@users.noreply.github.com>
2024-01-25 17:11:53 +0200

    Add files via upload

---
+++ scripts/influx/load_devops.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+results_dir="/home/user/influx/influx-flux-benchmarks/load_results"
+
+mkdir -p "${results_dir}"
+
+datasets=("devops_h10" "devops_d4" "devops_d16")
+workers=("1" "2" "4" "8")
+
+for dataset in "${datasets[@]}"; do
+  for worker in "${workers[@]}"; do
+    input_file="/mnt/data/influx/data/${dataset}.gz"
+    output_file="${results_dir}/influxql-w${worker}-${dataset}.txt"
+
+    cat "${input_file}" | gunzip | \
+    tsbs_load_influx --batch-size=1000 --workers="${worker}" \
+      --urls="http://localhost:8087/" --do-create-db=false \
+      --seed=0 \
+      --results-file="${output_file}"
+
+    echo "Completed: Workers=${worker}, Dataset=${dataset}"
+  done
+done
---
+++ scripts/timescale/single-node/load-helper-scripts/devops_h8.sh
@@ -0,0 +1,247 @@
+#!/bin/bash
+
+ # devops_h8  
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=1  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w1_p0_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000
+
+#  #workers:1 partitions:0 chunktime:variable 
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=1  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=2h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w1_p0_ct_2h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=1  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=1h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w1_p0_ct_1h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000
+
+ #workers:1 partitions:variable 
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=1  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w1_p2_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=2
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=1  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w1_p4_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=4
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=1  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w1_p8_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=8
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=1  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w1_p16_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=16
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=1  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w1_p32_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=32
+
+ #workers:2 partitions:0 chunktime:variable 
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=2  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w2_p0_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=2  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=2h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w2_p0_ct_2h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=2  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=1h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w2_p0_ct_1h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000
+
+ #workers:2 partitions:variable 
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=2  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w2_p2_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=2
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=2  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w2_p4_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=4
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=2  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w2_p8_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=8
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=2  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w2_p16_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=16
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=2  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w2_p32_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=32
+
+
+ #workers:4 partitions:0 chunktime:variable 
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=4  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w4_p0_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=4  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=2h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w4_p0_ct_2h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=4  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=1h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w4_p0_ct_1h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000
+
+
+ #workers:4 partitions:variable 
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=4  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w4_p2_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=2
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=4  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w4_p4_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=4
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=4  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w4_p8_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=8
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=4  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w4_p16_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=16
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=4 --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w4_p32_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=32
+
+
+
+ #workers:8 partitions:0 chunktime:variable 
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=8  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w8_p0_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=8  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=2h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w8_p0_ct_2h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=8  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=1h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w8_p0_ct_1h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000
+
+### #workers:2 partitions:variable 
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=8  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w8_p2_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=2
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=8  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w8_p4_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=4
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=8  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w8_p8_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=8
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=8  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w8_p16_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=16
+
+cat /mnt/data/timescale/data/devops_h8.gz | gunzip | tsbs_load_timescaledb \
+--host="localhost" --port=5432 --pass="postgres" \
+--user="postgres"  --workers=8  --log-batches=true \
+--in-table-partition-tag=false --chunk-time=8h --results-file="/home/user/timescale-setup/single-node/cpu-load-benchmarks-500/devops_h8/w8_p32_ct_8h_bs100k.txt" \
+--field-index-count=1 --do-create-db=true --force-text-format=false \
+--do-abort-on-exist=false --seed=0 --batch-size=100000 --partitions=32
